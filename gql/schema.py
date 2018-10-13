d0bb8191e1dcec1ce60680feb1a3e9db7df28bb6
David Blurton <david.blurton@gmail.com>
2018-10-13 00:30:17 +0000

    Implement basic queries

--- gql/schema.py
+++ gql/schema.py
@@ -1,18 +1,34 @@
 import graphene
-import models
+from models import *
 
 import gql.types as types
 
 
+class Stats(graphene.ObjectType):
+  lemmas = graphene.Int()
+  forms = graphene.Int()
+
+
 class Query(graphene.ObjectType):
     lemma = graphene.Field(types.Lemma, name=graphene.String())
-    forms = graphene.List(types.Form, search=graphene.String())
+    forms = graphene.List(types.Form, form=graphene.String())
+    search = graphene.List(types.Form, search=graphene.String())
+    stats = graphene.Field(Stats)
 
     def resolve_lemma(self, info, name):
-        return models.Lemma.get(name=name)
+        return Lemma.get(name=name)
 
-    def resolve_forms(self, info, search):
-        return models.find_forms(search)
+    def resolve_forms(self, info, form):
+        return find_forms(form)
+
+    def resolve_search(self, info, search):
+        return Form.select().where(Form.name.startswith(search)).limit(100)
+
+    def resolve_stats(self, info):
+        lemmas = Lemma.select().count()
+        forms = Form.select().count()
+
+        return Stats(lemmas=lemmas, forms=forms)
 
 
 schema = graphene.Schema(query=Query, types=[types.Lemma, types.Form])
--- gql/types.py
+++ gql/types.py
@@ -5,7 +5,17 @@ import models
 class Lemma(graphene.ObjectType):
     name = graphene.String()
     part_of_speech = graphene.String()
+    declensions = graphene.List(graphene.List(graphene.String))
 
+    def resolve_declensions(lemma, info):
+      forms = models.Form.select().join(models.Lemma).where(models.Lemma.id == lemma.id)
+
+      declensions = [f.name for f in forms]
+      group_size = len(declensions) // 4
+
+      grouped = zip(*(iter(declensions),) * group_size)
+      return list(map(list, zip(*grouped)))
 
 class Form(graphene.ObjectType):
     name = graphene.String()
+    head_word = graphene.Field(Lemma)
