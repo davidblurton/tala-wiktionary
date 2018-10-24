2acea1d0b42fe5d69443d04aac9fd321ef36da75
David Blurton <david.blurton@gmail.com>
2018-10-24 13:49:24 +0000

    Update to latest responder with fixed static behaviour

--- api.py
+++ api.py
@@ -2,14 +2,10 @@ import responder
 
 from gql.schema import schema
 
-api = responder.API(static_dir='build')
+api = responder.API(static_dir="build")
 
 api.add_route("/graphql", schema)
-# api.add_route("/", static=True)
-
-@api.route("/")
-async def frontend(req, resp):
-    resp.content = api.static_response(req, resp)
+api.add_route("/", static=True)
 
 if __name__ == "__main__":
-    api.run(address='0.0.0.0', port=5000)
+    api.run(address="0.0.0.0", port=5000)
