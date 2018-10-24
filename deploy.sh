16d5c47414453cf02cba4fa517a52a1427351855
David Blurton <david.blurton@gmail.com>
2018-10-24 19:24:02 +0000

    Add git tags to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-set -e
+set -euo pipefail
+
+: ${CIRCLE_SHA1:=$(git rev-parse --verify HEAD)}
+REGISTRY=davidblurton
+PROJECT=tala-graph-api
+TAG=$REGISTRY/$PROJECT:$CIRCLE_SHA1
+TAG_LATEST=$REGISTRY/$PROJECT:latest
 
 yarn run build
 
-docker build -t davidblurton/tala-graph-api:latest .
-docker push davidblurton/tala-graph-api:latest
+docker build -t $TAG_LATEST -t $TAG .
+docker push $TAG
+docker push $TAG_LATEST
 
 eval $(docker-machine env tala)
-docker service update --update-failure-action rollback --image latest tala_graphql
+docker service update --update-failure-action rollback --image $TAG tala_graphql
+docker service ps tala_graphql
 
 # scp -i ~/.ssh/digitalocean stack.yml root@pianogr.am:~/tala2.yml
